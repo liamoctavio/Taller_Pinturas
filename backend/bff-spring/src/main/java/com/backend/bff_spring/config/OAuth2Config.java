3528efec2457e70d539d8c3b0353635578b4d622
puffoide <fe.salgadot@duocuc.cl>
2025-12-11 01:27:01 -0300

    Se incorpora OAuth2Config para el cors

---
+++ backend/bff-spring/src/main/java/com/backend/bff_spring/config/OAuth2Config.java
@@ -0,0 +1,40 @@
+// package com.backend.bff_spring.config;
+
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+// import org.springframework.security.config.web.server.ServerHttpSecurity;
+// import org.springframework.security.web.server.SecurityWebFilterChain;
+
+// import org.springframework.web.cors.CorsConfiguration;
+// import org.springframework.web.cors.reactive.CorsConfigurationSource;
+// import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+// import java.util.List;
+
+// @Configuration
+// @EnableWebFluxSecurity
+// public class OAuth2Config {
+
+//     @Bean
+//     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+//         return http
+//             .csrf(csrf -> csrf.disable())
+//             .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+//             .authorizeExchange(exchanges -> exchanges.anyExchange().permitAll())
+//             .build();
+//     }
+
+//     @Bean
+//     CorsConfigurationSource corsConfigurationSource() {
+//         CorsConfiguration configuration = new CorsConfiguration();
+//         configuration.setAllowedOrigins(List.of("*"));
+//         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+//         configuration.setAllowedHeaders(List.of("*"));
+        
+//         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//         source.registerCorsConfiguration("/**", configuration);
+//         return source;
+//     }
+    
+//     // AQUÍ HABÍA UN @Bean WebClient... ¡YA NO ESTÁ! (BORRADO)
+// }
